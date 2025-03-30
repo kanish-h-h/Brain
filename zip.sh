24718fbc24a106f2eb10372233a809fdac6a718a
kanish-h-h <kanish.kumar0804@gmail.com>
2025-03-30 18:07:12 +0530

    ADD: zip files & bash script

---
+++ zip.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Get the directory where the script is located
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+FOLDER_NAME="$(basename "$SCRIPT_DIR")"
+ZIP_FILE="$SCRIPT_DIR/$FOLDER_NAME.zip"
+
+# Create a zip archive of the current folder
+zip -r "$ZIP_FILE" "$SCRIPT_DIR"
+
+echo "Folder '$FOLDER_NAME' has been compressed into '$ZIP_FILE'."
